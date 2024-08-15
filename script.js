f9abc5f9d6e54dc3d8b9c903b587285c837850f4
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2024-08-15 18:58:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,4 @@ document.getElementById('contactButton').addEventListener('click', function() {
     alert('Thank you for your interest! Please contact us at contact@example.com');
 });
 
-//Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
